2568510259f46ff68445fd0273919a53064891b1
Shubh7amydv <shubham.2023ug1109@iiitranchi.ac.in>
2026-07-31 00:22:50 +0530

    Configure STATIC_ROOT setting in Django settings.py for collectstatic deployment

--- backend/ERP-System-backend/backend/config/settings.py
+++ backend/ERP-System-backend/backend/config/settings.py
@@ -139,6 +139,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Media files (uploaded by users)
 MEDIA_URL = '/media/'
